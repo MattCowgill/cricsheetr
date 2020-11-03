77978a0b4fefd166125f75d7240addec356cde9d
Matt Cowgill <mattcowgill@gmail.com>
2020-11-03 16:03:23 +1100

    globals schmobals

--- R/read_cricsheet_csv.R
+++ R/read_cricsheet_csv.R
@@ -5,17 +5,18 @@
 #' @details Will not recognise where "penalty runs" have been awarded, unless
 #' they are included in extras.
 #' @param path Filepath to CSV
+#' @importFrom rlang .data
 
 read_cricsheet_csv <- function(path) {
   raw <- data.table::fread(path, skip = 1, fill = TRUE, sep = ",") %>%
     dplyr::as_tibble()
 
   info <- raw %>%
-    dplyr::filter(V1 == "info")
+    dplyr::filter(.data$V1 == "info")
 
   balls <- raw %>%
-    dplyr::filter(V1 == "ball") %>%
-    dplyr::select(-V1)
+    dplyr::filter(.data$V1 == "ball") %>%
+    dplyr::select(-.data$V1)
 
   # stopifnot(nrow(balls) + nrow(info) == nrow(raw))
 
@@ -40,14 +41,14 @@ read_cricsheet_csv <- function(path) {
     dplyr::select("series" = 2, "value" = 3)
 
   info <- info %>%
-    dplyr::arrange(series) %>%
-    dplyr::mutate(series = dplyr::case_when(dplyr::row_number() == 1 ~ series,
-                                            series == dplyr::lag(series) ~ paste(series, dplyr::row_number(), sep = "_"),
-                                            TRUE ~ series),
-                  series = dplyr::if_else(series == "team", "team_a", series))
+    dplyr::arrange(.data$series) %>%
+    dplyr::mutate(series = dplyr::case_when(dplyr::row_number() == 1 ~ .data$series,
+                                            .data$series == dplyr::lag(.data$series) ~ paste(.data$series, dplyr::row_number(), sep = "_"),
+                                            TRUE ~ .data$series),
+                  series = dplyr::if_else(.data$series == "team", "team_a", .data$series))
 
   comb <- info %>%
-    tidyr::spread(key = series, value = value) %>%
+    tidyr::spread(key = .data$series, value = .data$value) %>%
     dplyr::bind_cols(balls)
 
   comb
